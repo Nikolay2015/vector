5c070cec8476ec1bb94322399ad967577a1f4e7e
Nikolay2015 <mushroom8613@gmail.com>
2015-11-06 09:25:11 +0300

    newfiles

--- include/vector.h
+++ include/vector.h
@@ -4,152 +4,159 @@
 class Vector {
 
 public:
-  const static unsigned long n = 3;
+	const static unsigned long n = 3;
 
 Vector()
-  {
-		for (int i = 0; i < n; i++)
-			coords_[i] = 0;
-  };
+{
+	for (unsigned int i = 0; i < n; i++)
+		coords_[i] = 0;
+};
 
 explicit Vector(double number)
-  {
-		for (int i = 0; i < n; i++)
-			coords_[i] = number;
-	};
+{
+	for (unsigned int i = 0; i < n; i++)
+		coords_[i] = number;
+};
 
 Vector(const Vector &other)
-  {
-		for (int i = 0; i < n; i++)
-			coords_[i] = other.coords_[i];
-  };
+{
+	for (unsigned int i = 0; i < n; i++)
+		coords_[i] = other.coords_[i];
+};
 
 Vector &operator=(const Vector &other)
-  {
-		for (int i = 0; i < n; i++)
-			coords_[i] = other.coords_[i];
-  };
+{
+	for (unsigned int i = 0; i < n; i++)
+		coords_[i] = other.coords_[i];
+	return *this;
+};
+
 double operator[](unsigned long i) const
-	{
-		return coords_[i];
-	};
+{
+	return coords_[i];
+};
 
 double &operator[](unsigned long i)
-	{
-		return coords_[i];
-	};
+{
+	return coords_[i];
+};
 	
 Vector &operator+=(const Vector &other)
-  {
-		for (int i = 0; i < n; i++)
-			coords_[i] = coords_[i] + other.coords_[i];
-  };
+{
+	for (unsigned int i = 0; i < n; i++)
+		coords_[i] = coords_[i] + other.coords_[i];
+	return *this;
+};
 
 Vector &operator-=(const Vector &other)
-  {
-		for (int i = 0; i < n; i++)
-			coords_[i] = coords_[i] - other.coords_[i];
-  };
+{
+	for (unsigned int i = 0; i < n; i++)
+		coords_[i] = coords_[i] - other.coords_[i];
+	return *this;
+};
 
 Vector &operator*=(double number)
-  {
-		for (int i = 0; i < n; i++)
-			coords_[i] = coords_[i] * number;
-  };
+{
+	for (unsigned int i = 0; i < n; i++)
+		coords_[i] = coords_[i] * number;
+	return *this;
+};
 
 Vector &operator/=(double number)
-  {
-		for (int i = 0; i < n; i++)
-			coords_[i] = coords_[i] / number;
-  };
+{
+	for (unsigned int i = 0; i < n; i++)
+		coords_[i] = coords_[i] / number;
+	return *this;
+};
 
 friend bool operator==(const Vector &our, const Vector &other)
-  {
-		for (int i = 0; i < n; ++i)
-			if (our.coords_[i] != other.coords_[i])
-			{
-				return false;
-			}
-		return true;
-  };
+{
+	for (unsigned int i = 0; i < n; ++i)
+		if (our.coords_[i] != other.coords_[i])
+		{
+			return false;
+		}
+	return true;
+};
 
 friend Vector operator+(const Vector &our, const Vector &other)
-  {
-		Vector Vector_return;
-		for (int i = 0; i < n; i++)
-			Vector_return.coords_[i] = our.coords_[i] + other.coords_[i];
-		return Vector_return;
-  };
+{
+	Vector Vector_return;
+	for (unsigned int i = 0; i < n; i++)
+		Vector_return.coords_[i] = our.coords_[i] + other.coords_[i];
+	return Vector_return;
+};
 
 friend Vector operator-(const Vector &our, const Vector &other)
-  {
-		Vector Vector_return;
-		for (int i = 0; i < n; i++)
-			Vector_return.coords_[i] = our.coords_[i] - other.coords_[i];
-		return Vector_return;
-  };
+{
+	Vector Vector_return;
+	for (unsigned int i = 0; i < n; i++)
+		Vector_return.coords_[i] = our.coords_[i] - other.coords_[i];
+	return Vector_return;
+};
 
 friend Vector operator*(const Vector &our, double number)
-  {
-		Vector Vector_return;
-		for (int i = 0; i < n; i++)
-			Vector_return.coords_[i] = our.coords_[i] * number;
-		return Vector_return;
-  };
+{
+	Vector Vector_return;
+	for (unsigned int i = 0; i < n; i++)
+		Vector_return.coords_[i] = our.coords_[i] * number;
+	return Vector_return;
+};
 
 friend Vector operator*(double number, const Vector &our)
-  {
-		Vector Vector_return;
-		for (int i = 0; i < n; i++)
-			Vector_return.coords_[i] = our.coords_[i] * number;
-		return Vector_return;
-  };
+{
+	Vector Vector_return;
+	for (unsigned int i = 0; i < n; i++)
+		Vector_return.coords_[i] = our.coords_[i] * number;
+	return Vector_return;
+};
 
 friend Vector operator/(const Vector &our, double number)
-  {
-		Vector Vector_return;
-		for (int i = 0; i < n; i++)
-			Vector_return.coords_[i] = our.coords_[i] / number;
-		return Vector_return;
-  };
+{
+	Vector Vector_return;
+	for (unsigned int i = 0; i < n; i++)
+		Vector_return.coords_[i] = our.coords_[i] / number;
+	return Vector_return;
+};
   
 void print()
-	{
-		for(int i = 0; i < n; i++)
-			std::cout<<coords_[i]<<"   ";
-		std::cout<<'\n';
-	};
+{
+	for (unsigned int i = 0; i < n; i++)
+		std::cout<<coords_[i]<<"   ";
+	std::cout<<'\n';
+};
 	
 friend double operator^(const Vector &our, const Vector &other)
-	{
-		double sum[n];
-		double sum_return = 0;
-		for(int i = 0; i < n; i++)
-			{
-				sum[i] = our.coords_[i]*other.coords_[i];
-				sum_return = sum_return + sum[i];
-			}
-		return sum_return;
-	};
+{
+	double sum[n];
+	double sum_return = 0;
+	for (unsigned int i = 0; i < n; i++)
+		{
+			sum[i] = our.coords_[i]*other.coords_[i];
+			sum_return = sum_return + sum[i];
+		}
+	return sum_return;
+};
 
 Vector operator-()const
-	{
-		Vector Vector_return;
-		for (int i = 0; i < n; i++)
-			Vector_return.coords_[i] = (-1)*coords_[i] ;
-		return Vector_return;
-  };
+{
+	Vector Vector_return;
+	for (unsigned int i = 0; i < n; i++)
+		Vector_return.coords_[i] = (-1)*coords_[i] ;
+	return Vector_return;
+};
   
 private:
-  double coords_[n];
+	double coords_[n];
   
 }; // class Vector
 
 bool operator!=(const Vector &our, const Vector &other)
-	{
-		if (our == other)
-			return false;
-		else
-			return true;
-	};
+{
+	if (our == other)
+		return false;
+	else
+		return true;
+};
+
 #endif // VECTOR_H
